f7d961fe42ac766311df838094edc0abeb35493d
Satyam Ghodasara <sghodas@gmail.com>
2014-06-21 15:36:34 -0400

    Added custom permission checks

--- middleware/permission.js
+++ middleware/permission.js
@@ -1,7 +1,3 @@
-//===============
-//    Middleware
-//===============
-
 //    The order of this array matters - the indexes are used to rank permission levels
 var permissions = ['user', 'admin'];
 
@@ -12,20 +8,37 @@ var invalidPermissionError = function() {
     return err;
 };
 
-var requireMinLevel = function(requiredPermissionLevel) {
+
+//===============
+//    Middleware
+//===============
+
+var minLevel = function(requiredPermissionLevel) {
     return function(req, res, next) {
         var requiredRank = permissions.indexOf(requiredPermissionLevel);
         var accountRank = permissions.indexOf(req.account.permission);
         if (accountRank >= requiredRank) {
             next();
         } else {
-            res.send(403, JSON.stringify(invalidPermissionError(), ['name', 'message']));
+            res.send(403, JSON.stringify(invalidPermissionError(), ['name', 'message', 'detail']));
+        }
+    };
+};
+
+var check = function(block) {
+    return function(req, res, next) {
+        var err = block();
+        if (err) {
+            res.send(err.httpStatusCode || 403, JSON.stringify(err, ['name', 'message', 'detail']));
+        } else {
+            next();
         }
     };
 };
 
 module.exports = {
-    requireMinLevel: requireMinLevel,
+    minLevel: minLevel,
+    check: check,
     levels: {
         USER: 'user',
         ADMIN: 'admin'
